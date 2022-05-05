21b7277a46e268f9bfa2af5601ed6f1f8e2109c3
caesar699 <caesar699@gmail.com>
2022-05-05 16:39:08 +0600

    apps

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 import SecondComponent from "./Components/SecondComponent";
+import React from 'react';
 
 function App() {
   function MyComponent() {
-    return <h1>This is my First Component</h1>;
+    return (
+      <React.Fragment>
+        <h1>This is my First Component</h1>
+      <p>This paragraph is part of component</p>
+      <li>This is li type</li>
+      </React.Fragment>
+      
+    )
   }
   return (
     <div>
