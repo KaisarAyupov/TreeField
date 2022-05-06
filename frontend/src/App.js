b53feb523f6976c63baf115dee758b1914ece0e4
caesar699 <caesar699@gmail.com>
2022-05-06 10:58:09 +0600

    Looping through an array of data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import AppleComponent from './Components/AppleComponent';
-
-import SecondComponent from "./Components/SecondComponent";
+import Fruit from './Components/Fruit';
 import React from 'react';
 
+const   theFruits = [
+  {id: 1, name: "Apple", color: "red"},
+  {id: 2, name: "Orange", color: "orange"},
+  {id: 3, name: "Banana", color: "yellow"},
+  {id: 4, name: "Kiwi", color: "green"},
+  {id: 5, name: "Mango", color: "red"},
+  {id: 6, name: "Avocado", color: "green"},
+  ]
+
 function App() {  
-  return <AppleComponent />
+  return (
+    <>
+    {/* <Fruit color="yelow" name="banana" />
+    <Fruit color="red" name="aple" /> */}
+    {theFruits.map((fruit)=>{
+      return <Fruit key={fruit.id} color={fruit.color} name={fruit.name} />
+    })}
+    </>
+  )
 }
 
 export default App;
