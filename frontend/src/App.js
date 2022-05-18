d961d4de46d6df348ffa1f742b2c1e0ebe92f1fd
caesar699 <caesar699@gmail.com>
2022-05-18 16:34:23 +0600

    Section2: router dom use component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,18 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import logo from './logo.svg';
-import './App.css';
-import AppleComponent from './Components/AppleComponent';
-import React from 'react';
 
+import React from 'react';
+ // Components
+ import Home from './Components/Home';
+ import Listings from './Components/Listings';
+ import Login from './Components/Login';
 
 function App() {  
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<h1>This is the homepage</h1>}/>
-        <Route path='/login' element={<h1>This is the loginpage</h1>}/>
-        <Route path='/listings' element={<h1>This is the property listings page</h1>}/>
+        <Route path='/' element={<Home />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/listings' element={<Listings />}/>
         
        
         
