7b42fdfb05e7fed60df35c5eef215eb94e647ef5
caesar699 <caesar699@gmail.com>
2022-05-19 14:54:54 +0600

    Section3: Page Layout

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
-import { Button, Typography } from '@mui/material';
+import { Button, Typography, Grid } from '@mui/material';
 import {makeStyles} from '@mui/styles';
-
+import CustomCard from './CustomCard';
+import CssBaseline from '@mui/material/CssBaseline';
 
 const useStyles = makeStyles ({
   divStyle: {
@@ -21,22 +22,24 @@ function Home() {
   const classes = useStyles();
   return (
     <>
-        <div className={classes.divStyle}>
-        <Typography align='riht' variant='h4'>  This is the Title </Typography>
-        <Typography align='justify' variant='body1'>  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </Typography>
-
-        <Button 
-        onClick={()=>setBtnColor("success")} 
-        className={classes.btnStyle}
-        size="large" 
-        variant="contained"
-        >
-          Go
-        </Button>
-
-        </div>
-    </>    
-  )
+      <CssBaseline />
+      <Grid container>
+        <Grid item xs={12} md={6}>
+          <CustomCard />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <CustomCard />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <CustomCard />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <CustomCard />
+        </Grid>
+      </Grid>
+    </>
+    
+  )  
 }
 
 export default Home
