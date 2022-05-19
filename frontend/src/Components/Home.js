246c720784d12e2d14880bae0a435300ff4aa264
caesar699 <caesar699@gmail.com>
2022-05-19 15:10:16 +0600

    Section3: conteiner grid

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -23,18 +23,22 @@ function Home() {
   return (
     <>
       <CssBaseline />
-      <Grid container>
-        <Grid item xs={12} md={6}>
-          <CustomCard />
+      <Grid 
+      direction="column"
+      justifyContent="space-evenly"
+      alignItems="flex-start"
+      container 
+      style={{height: "100vh", backgroundColor: "grey"}}>
+        <Grid item container>
+          <Grid item xs={5}>
+            <CustomCard />
+          </Grid>
         </Grid>
-        <Grid item xs={12} md={6}>
-          <CustomCard />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <CustomCard />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <CustomCard />
+        
+        <Grid item container>
+          <Grid item xs={5}>
+            <CustomCard />
+          </Grid>
         </Grid>
       </Grid>
     </>
