ad1c46c9c7bba70367075f74601e0f9d06f42cea
caesar699 <caesar699@gmail.com>
2022-05-19 11:27:53 +0600

    Section3: Typography

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,18 +1,27 @@
 import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
-import { Button } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 
 function Home() {
   const [btnColor, setBtnColor] = useState("error")
   return (
-    <Button 
-    onClick={()=>setBtnColor("success")} 
-    color={btnColor} 
-    size="large" 
-    variant="contained"
-    >
-      Turn Green
-    </Button>
+    <>
+    <div style={{width: '50%', border: "2px solid black", margin: "15px"}}>
+      <Typography align='riht' variant='h4'>  This is the Title </Typography>
+      <Typography align='justify' variant='body1'>  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </Typography>
+
+      <Button 
+      onClick={()=>setBtnColor("success")} 
+      color={btnColor} 
+      size="large" 
+      variant="contained"
+      >
+        Go
+      </Button>
+
+    </div>
+    
+    </>    
   )
 }
 
