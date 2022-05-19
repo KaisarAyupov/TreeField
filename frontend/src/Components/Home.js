dbb716176e70075576bd3446402028191274d148
caesar699 <caesar699@gmail.com>
2022-05-19 10:58:03 +0600

    Section3: MUI component property

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
 import { Button } from '@mui/material';
 
 function Home() {
+  const [btnColor, setBtnColor] = useState("error")
   return (
-    <Button variant="outlined">Outlined</Button>
+    <Button 
+    onClick={()=>setBtnColor("success")} 
+    color={btnColor} 
+    size="large" 
+    variant="contained"
+    >
+      Turn Green
+    </Button>
   )
 }
 
