b368982deb4e1ce140d6e97ff3f1fd77229d6d35
caesar699 <caesar699@gmail.com>
2022-05-20 11:22:45 +0600

    Section4: Marker pozition

--- frontend/src/Components/Listings.js
+++ frontend/src/Components/Listings.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 // react-leaflet
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import { Icon } from 'leaflet';
@@ -28,28 +28,36 @@ function Listings() {
     iconUrl: officeIconpng,
     iconSize: [40, 40],
   })
+
+  const [lattude, setLatitude] = useState(43.2611)
+  const [longitude, setLongitude] = useState(76.8822)
+
+  function GoEast(){
+    setLatitude(43.2399);
+    setLongitude(76.8853);
+  }
+  function Gocenter(){
+    setLatitude(43.2611);
+    setLongitude(76.8822);
+  }
   return (
     <Grid container>
       <Grid item xs={4}>
-        <Typography variant='h1'>
-          Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-          The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-
-        </Typography>
+        <Button onClick={GoEast}>Go East</Button>
+        <Button onClick={Gocenter}>Go East</Button>
         Blank Space
       </Grid>
       <Grid item xs={8}>
         <AppBar position='sticky'>
           <div style={{ height: "100vh" }}>
-            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+            <MapContainer center={[43.2611, 76.8822]} zoom={13} scrollWheelZoom={false}>
               <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               />
               <Marker
               icon={houseIcon}
-              position={[51.505, -0.09]}>
+              position={[lattude, longitude]}>
                 {<Popup>
                   <Typography variant='h5'>
                     A title
@@ -58,7 +66,9 @@ function Listings() {
                   <Typography variant='body1'>
                     Thi is som text
                   </Typography>
-                  <Button variant='contained' fullWidth>A link</Button>
+                  <Button variant='contained' fullWidth>
+                    A link
+                  </Button>
                 </Popup>}
               </Marker>
             </MapContainer>
