0bbc662b06e9ba763bde372006dd169e7cb4fcba
caesar699 <caesar699@gmail.com>
2022-05-23 16:09:46 +0600

    Section5: Listings model

---
+++ backend/listings/models.py
@@ -0,0 +1,46 @@
+from random import choices
+from django.db import models
+from django.utils import timezone
+
+class Listing(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField(null=True, blank=True)
+    choices_area = (
+        ('Inner Almaty', 'Inner Almaty'),
+        ('Outer Almaty', 'Outer Almaty'),        
+    )
+    area = models.CharField(max_length=200, blank=True, null=True, choices=choices_area) # area
+    borough = models.CharField(max_length=50, blank=True, null=True) # borough
+    choices_listing_type = (
+        ('House', 'House'),
+        ('Apatment', 'Apatment'),
+        ('Office', 'Office'),
+    )
+    listing_type = models.CharField(max_length=20, choices=choices_listing_type)    
+    choices_property_status = (
+        ('Critically Endangered', 'Critically Endangered'),
+        ('Endangered', 'Endangered'),
+        ('Vulnerable', 'Vulnerable'),
+    )
+    property_status = models.CharField(max_length=20, blank=True, choices=choices_property_status)
+    price = models.DecimalField(max_digits=50, decimal_places=0 )
+    choices_rental_frequency = (
+        ('Month', 'Month'),
+        ('Week', 'Week'),
+        ('Day', 'Day'),
+    )
+    rental_frequency = models.CharField(max_length=20, null=True, choices=choices_rental_frequency)
+    rooms = models.IntegerField(blank=True)
+    finished = models.BooleanField(default=False)
+    pool = models.BooleanField(default=False)
+    elevator = models.BooleanField(default=False)
+    cctv = models.BooleanField(default=False)
+    parking = models.BooleanField(default=False)
+    data_posted = models.DateTimeField(default=timezone.now)
+
+
+
+
+
+
+    
