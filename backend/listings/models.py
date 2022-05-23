619284091fd3d8a853ddd20baacf931d339f207a
caesar699 <caesar699@gmail.com>
2022-05-23 16:21:52 +0600

    Section5: Location field

--- backend/listings/models.py
+++ backend/listings/models.py
@@ -1,6 +1,7 @@
 from random import choices
-from django.db import models
+from django.contrib.gis.db import models
 from django.utils import timezone
+from django.contrib.gis.geos import Point
 
 class Listing(models.Model):
     title = models.CharField(max_length=150)
@@ -37,6 +38,7 @@ class Listing(models.Model):
     cctv = models.BooleanField(default=False)
     parking = models.BooleanField(default=False)
     data_posted = models.DateTimeField(default=timezone.now)
+    location = models.PointField(blank=True, null=True, SRID=4326)
 
 
 
