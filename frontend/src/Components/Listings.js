d8a4726495540a9b01a4085054c1ae0326a48f98
caesar699 <caesar699@gmail.com>
2022-05-24 14:28:03 +0600

    Section6: Making reqest

--- frontend/src/Components/Listings.js
+++ frontend/src/Components/Listings.js
@@ -40,6 +40,7 @@ const useStyles = makeStyles({
 });
 
 function Listings() {
+  fetch('http://127.0.0.1:8000/api/listings/').then(response=> response.json()).then(data=>console.log(data))
   const classes = useStyles();
   const houseIcon = new Icon({
     iconUrl: houseIconpng,
