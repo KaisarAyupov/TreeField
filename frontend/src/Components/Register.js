ff87ac8524b4406994893fa0140e356dd3693023
caesar699 <caesar699@gmail.com>
2022-05-25 17:00:39 +0600

    User registration

--- frontend/src/Components/Register.js
+++ frontend/src/Components/Register.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { useNavigate } from 'react-router-dom';
+import Axios from "axios";
 // MUI
 import { Grid, AppBar, Typography, Button, Card, CardHeader, CardMedia, CardContent, CircularProgress, TextField } from '@mui/material';
 import { makeStyles } from '@mui/styles';
@@ -29,9 +30,45 @@ const useStyles = makeStyles({
 function Register() {
     const classes = useStyles();
     const navigate = useNavigate();
+    const [sendRequest, setSendRequest] = useState(false);
+
+    function FormSubmit(e){
+      e.preventDefault();
+      console.log("Test");
+      setSendRequest(!sendRequest)
+      
+    }
+    useEffect(() => {
+      if (sendRequest) {
+        const source = Axios.CancelToken.source();
+        async function SignUp() {
+          try {
+            const response = await Axios.post(
+              "http://localhost:8000/api-auth-djoser/users/",
+              {
+                username: "testinguser",
+                email: "testinguser@gmail.com",
+                password: "mypass123",
+                re_password: "mypass123",
+              },
+              {
+                cancelToken: source.token
+              }
+            );
+            console.log(response)          
+          } catch (error) {
+            console.log(error);
+          }
+        }
+        SignUp();
+        return () => {
+          source.cancel();
+        };
+      }
+    }, [sendRequest])
   return (
     <div className={classes.formConteiner}>
-        <form>
+        <form onSubmit={FormSubmit}>
             <Grid item container style={{ marginTop: '1rem'}}>
             <Typography variant='h4'>CREATE AN ACCOUNT</Typography>
             </Grid>
