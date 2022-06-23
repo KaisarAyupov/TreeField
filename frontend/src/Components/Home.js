222cc2a137dc993b2a95c006102824b05a74823c
caesar699 <caesar699@gmail.com>
2022-06-23 16:49:54 +0600

    Home: Wall paper

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -4,59 +4,54 @@ import {Link} from 'react-router-dom'
 // MUI
 import { Button, Typography, Grid, AppBar, Toolbar } from '@mui/material';
 import {makeStyles} from '@mui/styles';
-
+import ProductHeroLayout from './ProductHeroLayout';
 // Components
 import CustomCard from './CustomCard';
 
 // Assets
 import city from './Assets/city.jpg';
-
-const useStyles = makeStyles ({
-    cityImg: {
-    width: "100%",
-    height: "92vh",
-  },
-  overlayText: {
-    position: "absolute",
-    zIndex: "100",
-    top: "100px",
-    left: "20px",
-    textAlign: "center",
-  },
-  homeText: {
-    color: "white",
-    fontWeight: "bolder",
-  },
-  homeBtn: {
-    fontSize: "3.5rem",
-    borderRadius: "15px",
-    backgroundColor: "green",
-    marginTop: "2rem",
-    boxShadow: "3px 3px 3px white"
-  }
-
-});
-
-function Home() {
-  const [btnColor, setBtnColor] = useState("error");
-  const classes = useStyles();
+const backgroundImage =
+  'https://images.unsplash.com/photo-1534854638093-bada1813ca19?auto=format&fit=crop&w=1400';
+  
+export default function Home() {
   return (
-    <>      
-      <div style={{position: 'relative'}}>
-        <img src={city} className={classes.cityImg} />
-        <div className={classes.overlayText} >
-          <Typography variant='h1' className={classes.homeText}>
-            FIND YOUR NEXT <span style={{color: 'green'}}>POINT</span>
-          </Typography>
-          <Button variant='contained'className={classes.homeBtn}>
-            SEE ALL POINTS
-          </Button>
-        </div>
-      </div>
-      
-    </>
-    
-  )  
+    <ProductHeroLayout
+      sxBackground={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundColor: '#7fc7d9', // Average color of the background image.
+        backgroundPosition: 'center',
+      }}
+    >
+      {/* Increase the network loading priority of the background image. */}
+      <img
+        style={{ display: 'none' }}
+        src={backgroundImage}
+        alt="increase priority"
+      />
+      <Typography color="inherit" align="center" variant="h2" marked="center">
+      i29zx8hy
+      </Typography>
+      <Typography
+        color="inherit"
+        align="center"
+        variant="h5"
+        sx={{ mb: 4, mt: { sx: 4, sm: 10 } }}
+      >
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry..
+      </Typography>
+      <Button
+        color="secondary"
+        variant="contained"
+        size="large"
+        component="a"
+        href="/"
+        sx={{ minWidth: 200 }}
+      >
+        Register
+      </Button>
+      <Typography variant="body2" color="inherit" sx={{ mt: 2 }}>
+        Discover the experience
+      </Typography>
+    </ProductHeroLayout>
+  );
 }
-
-export default Home
