06dce4fb04db76303cfabb482ae63fde081bd683
caesar699 <caesar699@gmail.com>
2022-07-04 13:41:09 +0600

    Persistent drawer

--- frontend/src/Components/Contacts.js
+++ frontend/src/Components/Contacts.js
@@ -1,263 +1,173 @@
-import React, { useState, useEffect } from 'react'
-import Axios from "axios";
-import {useImmerReducer} from 'use-immer';
-import { useNavigate } from 'react-router-dom';
-// react-leaflet
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
-import { Icon } from 'leaflet';
-// MUI
-import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import MuiDrawer from '@mui/material/Drawer';
+import * as React from 'react';
+import { styled, useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
 import MuiAppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
-import CircularProgress from '@mui/material/CircularProgress';
 import IconButton from '@mui/material/IconButton';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import {Card, CardHeader, CardMedia, CardContent, CardActions} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import RoomIcon from '@mui/icons-material/Room';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
 
-// Map icons
-import houseIconpng from './Assets/Mapicons/house.png';
-import apartmentIconpng from './Assets/Mapicons/trash_bin32.png';
-import officeIconpng from './Assets/Mapicons/office.png';
+const drawerWidth = 240;
 
-const drawerWidth = 400;
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  }),
+);
 
 const AppBar = styled(MuiAppBar, {
   shouldForwardProp: (prop) => prop !== 'open',
 })(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
+  transition: theme.transitions.create(['margin', 'width'], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
   ...(open && {
-    marginLeft: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
   }),
 }));
 
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
-    '& .MuiDrawer-paper': {
-      position: 'relative',
-      whiteSpace: 'nowrap',
-      width: drawerWidth,
-      transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-      boxSizing: 'border-box',
-      ...(!open && {
-        overflowX: 'hidden',
-        transition: theme.transitions.create('width', {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.leavingScreen,
-        }),
-        width: theme.spacing(7),
-        [theme.breakpoints.up('sm')]: {
-          width: theme.spacing(9),
-        },
-      }),
-    },
-  }),
-);
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
 
-const mdTheme = createTheme();
+export default function PersistentDrawerLeft() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
 
-function Contacts() {
-  const [open, setOpen] = React.useState(true);
-  const toggleDrawer = () => {
-    setOpen(!open);
+  const handleDrawerOpen = () => {
+    setOpen(true);
   };
 
-  const navigate = useNavigate();
-  const houseIcon = new Icon({
-    iconUrl: houseIconpng,
-    iconSize: [40, 40],
-  })
-  const apartmentIcon = new Icon({
-    iconUrl: apartmentIconpng,
-    iconSize: [32, 32],
-  })
-  const officeIcon = new Icon({
-    iconUrl: officeIconpng,
-    iconSize: [40, 40],
-  })
-
-  const [lattude, setLatitude] = useState(43.2611)
-  const [longitude, setLongitude] = useState(76.8822)
-
-  const initialState = {
-    mapInstance: null,
+  const handleDrawerClose = () => {
+    setOpen(false);
   };
-
-function ReduserFunction(draft, action) {
-    switch (action.type) {
-        case 'getMap':
-            draft.mapInstance = action.mapData;
-            break;
-
-    }
-
-}
-const [state, dispatch] = useImmerReducer(ReduserFunction, initialState)
-
-function TheMapComponent() {
-    const map = useMap();
-    dispatch({ type: "getMap", mapData: map });
-    return null;
-}  
-  const [AllListings, setAllListings] = useState([])
-  const [dataIsLoading, setDataIsLoading] = useState(true)
-
-  useEffect(() => {
-    const source =Axios.CancelToken.source();
-    async function GetAllListings(){
-      try {
-        const response = await Axios.get('http://127.0.0.1:8000/api/listings/', {cancelToken: source.token});
-      // console.log(response.data)
-      setAllListings(response.data)
-      setDataIsLoading(false)
-      } catch(error){
-        console.log(error)
-      }
-    }
-    GetAllListings();
-    return ()=>{
-      source.cancel();
-    }
-  }, [])
-
-  if (dataIsLoading === false) {
-    console.log(AllListings[0].location)
-  }
-  if (dataIsLoading === true) {
-    return (
-      <Grid
-        container
-        justifyContent='center'
-        alignItems='center'
-        style={{ height: '100vh' }}
-      >
-        <CircularProgress />
-      </Grid>
-    );    
-  }
-
-
-
-
 
   return (
-    <ThemeProvider theme={mdTheme}>
-      <Box sx={{ display: 'flex' }}>
-        <CssBaseline />
-        <AppBar position="absolute" open={open}>
-          
-        </AppBar>
-        <Drawer variant="permanent" open={open}>
-          <Toolbar
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'flex-end',
-              px: [1],
-            }}
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
           >
-            <IconButton onClick={toggleDrawer}>
-              <ChevronLeftIcon />
-            </IconButton>
-          </Toolbar>
-          <Divider />
-          <Grid item xs={4} style={{marginTop: "0.5rem"}}>
-        {AllListings.map((listing) => {
-          return (
-            <Card key={listing.id} >
-              <CardHeader
-                action={
-                  <IconButton
-                    aria-label="settings"
-                    onClick={() =>
-                      state.mapInstance.flyTo(
-                        [listing.lat, listing.lng],
-                        16
-                      )
-                    }
-                  >
-                    <RoomIcon />
-                  </IconButton>
-                }
-                title={listing.title}
-              />
-              <CardMedia
-              
-                component="img" height="194"
-                image={listing.picture1}
-                alt={listing.title}
-                onClick={()=>navigate(`/listings/${listing.id}`)}
-              />
-              <CardContent>
-                <Typography variant="body2">
-                  {listing.description.substring(0, 150)}...
-                </Typography>
-              </CardContent>
-              {listing.property_status === "Sale" ? (
-                <Typography >
-                  {listing.listing_type}:  
-                {listing.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-              </Typography>
-              ): (
-                <Typography>
-                {listing.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} / {listing.rental_frequency }
-              </Typography>
-              )}
-              
-              <CardActions disableSpacing>
-                <IconButton aria-label="add to favorites">
-                  {listing.seller_agency_name}
-                </IconButton>
-              </CardActions>
-            </Card>
-          )
-        })}
-      </Grid>
-        </Drawer>
-        <Box
-          component="main"
-          sx={{
-            backgroundColor: (theme) =>
-              theme.palette.mode === 'light'
-                ? theme.palette.grey[100]
-                : theme.palette.grey[900],
-            flexGrow: 1,
-            height: '90vh',
-            overflow: 'auto',
-          }}
-        >
-          <Toolbar />
-          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-            <Grid container spacing={3}>
-              <Typography>Test</Typography>              
-            </Grid>
-          </Container>
-        </Box>
-      </Box>
-
-    </ThemeProvider>
-    
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+            
+          ))}
+        </List>        
+      </Drawer>
+      <Main open={open}>
+        <DrawerHeader />
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
+          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
+          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
+          Convallis convallis tellus id interdum velit laoreet id donec ultrices.
+          Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
+          nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
+          leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
+          feugiat vivamus at augue. At augue eget arcu dictum varius duis at
+          consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
+          sapien faucibus et molestie ac.
+        </Typography>
+        <Typography paragraph>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
+          eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
+          neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
+          tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
+          sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
+          tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
+          gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
+          et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
+          tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+          posuere sollicitudin aliquam ultrices sagittis orci a.
+        </Typography>
+      </Main>
+    </Box>
   );
 }
-
-export default Contacts
